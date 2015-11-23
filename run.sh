f2951e05fd29a273f71c04e4ca7d0f0405af6bdb
Marina Fomicheva <mari.fomicheva@gmail.com>
2015-11-23 11:30:35 +0000

    git test

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
+#!/usr/bin/env bash
+
 java -mx5g -cp "$STANFORD_CORENLP_DIR/*" edu.stanford.nlp.pipeline.StanfordCoreNLP -outputFormat text -outputDirectory "$1" -props "/Users/MarinaFomicheva/workspace/stanford-corenlp-python/tok.properties" -filelist "/Users/MarinaFomicheva/workspace/stanford-corenlp-python/fileList.txt"
