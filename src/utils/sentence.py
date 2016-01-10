42cd3793ea4aeea7a5a9cdfd783b6a0d71f40a04
Marina Fomicheva <mari.fomicheva@gmail.com>
2016-01-10 13:16:14 +0000

    Added methods for getting metrics scores and quest features to sentence object

--- src/utils/sentence.py
+++ src/utils/sentence.py
@@ -1,20 +1,41 @@
 __author__ = 'MarinaFomicheva'
 
+import re
+from src.utils.features_reader import FeaturesReader
+
 class Sentence(object):
 
     def __init__(self):
-        self.parse = object
-        self.alignments = object
-        self.lm = object
+        self.parse = []
+        self.alignments = []
+        self.quest_word = []
+        self.quest_sent = []
+        self.bleu = []
+        self.meteor = []
 
     def add_alignments(self, alignments):
         self.alignments = alignments
-
-    def add_lm(self, lm):
-        self.lm = lm
 
     def add_parse(self, parse):
         self.parse = parse
 
     def add_quest_word(self, quest_word):
         self.quest_word = quest_word
+
+    def add_quest_sent(self, quest_sent):
+        self.quest_sent = quest_sent
+
+    def add_bleu(self, scores_file):
+
+        for line in open(scores_file).readlines():
+            if not line.startswith('  BLEU'):
+                continue
+            self.bleu.append(float(re.sub(r'^.+= ([01]\.[0-9]+).+$', r'\1', line.strip())))
+
+    def add_meteor(self, scores_file):
+
+        for line in open(scores_file).readlines():
+            if not line.startswith('Segment '):
+                continue
+            self.meteor.append(float(line.strip().split('\t')[1]))
+
