c25d4762e101f3d98036627d6aeeb4f79d18bece
Marina Fomicheva <mari.fomicheva@gmail.com>
2016-01-10 13:13:28 +0000

    Added method for reading the names of quest features from quest configuration file

---
+++ src/utils/features_reader.py
@@ -0,0 +1,33 @@
+__author__ = 'MarinaFomicheva'
+
+import xml.etree.ElementTree as ET
+import os
+
+
+class FeaturesReader(object):
+
+    @staticmethod
+    def read_features(file_):
+
+        tree = ET.parse(file_)
+        root = tree.getroot()
+
+        features = []
+
+        for f in root.findall('feature'):
+            features.append(f.get('index'))
+
+        return features
+
+
+def main():
+
+    my_file = os.path.expanduser('~/Dropbox/workspace/questplusplus/config/features/my_features_fluency_sentence_level.xml')
+    reader = FeaturesReader()
+    features = reader.read_features(my_file)
+    for f in features:
+        print f
+
+
+if __name__ == '__main__':
+    main()
