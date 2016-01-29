ad59749baf9c522d0a4d905da9db059e99431cf0
Marina Fomicheva <mari.fomicheva@gmail.com>
2016-01-29 14:59:58 +0000

    Updated according to changes in sentence object and feature extractor

--- src/predictor/absolute_scores.py
+++ src/predictor/absolute_scores.py
@@ -1,8 +1,10 @@
 __author__ = 'MarinaFomicheva'
 
 import os
+from ConfigParser import ConfigParser
 from src.utils import sample_dataset
 from src.features.feature_extractor import FeatureExtractor
+from src.tools.run_tools import RunTools
 from sklearn import cross_validation as cv
 from src.learning import learn_model
 import numpy as np
@@ -14,56 +16,28 @@ class PredictorAbsoluteScores():
         self.data_size = int
         self.paths = {}
 
-    def set_paths(self):
-
-        self.paths['f_src_plain'] = os.getcwd() + '/' + 'data' + '/' + 'wmt13_graham' + '/' + 'source.dummy'
-        self.paths['f_tgt_plain'] = os.getcwd() + '/' + 'data' + '/' + 'wmt13_graham' + '/' + 'system'
-        self.paths['f_tgt_parse'] = os.getcwd() + '/' + 'data' + '/' + 'wmt13_graham' + '/' + 'system.parse'
-        self.paths['f_ref_plain'] = os.getcwd() + '/' + 'data' + '/' + 'wmt13_graham' + '/' + 'reference'
-        self.paths['f_ref_parse'] = os.getcwd() + '/' + 'data' + '/' + 'wmt13_graham' + '/' + 'reference.parse'
-        self.paths['f_eval'] = '/Users/MarinaFomicheva/Dropbox/workspace/dataSets/wmt13-manual-evaluation/continuous/z-seg-scrs/sample.seg.ad.stnd.all-en.csv'
-        self.paths['f_train_phr'] = os.getcwd() + '/' + 'data' + '/' + 'wmt13_graham' + '/' + 'train_phr.txt'
-        self.paths['f_test_phr'] = os.getcwd() + '/' + 'data' + '/' + 'wmt13_graham' + '/' + 'test_phr.txt'
-        self.paths['f_config'] = os.getcwd() + '/' + 'config' + '/' + 'learner' + '/' + 'svr.cfg'
-
-        self.paths['out_dir'] = os.getcwd() + '/' + 'results' + '/' + 'wmt13_graham'
-
     def set_data_size(self, size):
         self.data_size = size
 
-    def run(self, prefix='cobalt_word'):
-
-        self.set_paths()
-
-        # Create train and test samples from dataset
-
-        self.create_samples(1120)
-
-        # Extract features
+    def run(self, prefix, config, features_to_extract):
 
         for sample_name in ['train', 'test']:
             my_prefix = ''
             if len(prefix) > 0:
                 my_prefix = prefix + '_'
 
-            output_path = self.paths['out_dir'] + '/' + my_prefix + 'features' + '.' + sample_name + '.tsv'
+            output_path = config.get('Data', 'output') + '/' + my_prefix + 'features' + '.' + sample_name + '.tsv'
             if os.path.exists(output_path):
                 print "Feature files already exist!"
                 break
-            output = open(output_path, 'w')
 
-            extractor = FeatureExtractor()
-            extractor.read_config()
-            extractor.get_feature_names()
-            extractor.extract_features(src_plain=self.paths['f_src_plain'] + '.' + sample_name,
-                                       tgt_plain=self.paths['f_tgt_plain'] + '.' + sample_name,
-                                       ref_plain=self.paths['f_ref_plain'] + '.' + sample_name,
-                                       tgt_parse=self.paths['f_tgt_parse'] + '.' + sample_name,
-                                       ref_parse=self.paths['f_ref_parse'] + '.' + sample_name,
-                                       align_dir=os.getcwd() + '/' + 'alignments' + '/' + 'wmt13_graham',
-                                       aligner='cobalt',
-                                       baseline_dir=os.getcwd() + '/' + 'baselines' + '/' + 'wmt13_graham'
-            )
+            tools = RunTools(config)
+            sents_tgt, sents_ref = tools.assign_data(sample_name)
+
+            extractor = FeatureExtractor(config)
+            extractor.extract_features(features_to_extract, sents_tgt, sents_ref)
+
+            output = open(output_path, 'w')
 
             for phr in extractor.vals:
                 output.write('\t'.join([str(x) for x in phr]) + '\n')
@@ -72,30 +46,17 @@ class PredictorAbsoluteScores():
 
         # Learn model
 
-        predicted = learn_model.run(self.paths['f_config'],
-                                x_train_path=self.paths['out_dir'] + '/' + my_prefix + 'features' + '.' + 'train' + '.tsv',
-                                x_test_path=self.paths['out_dir'] + '/' + my_prefix + 'features' + '.' + 'test' + '.tsv',
-                                y_train_path=self.paths['f_eval'] + '.' + 'train',
-                                y_test_path=self.paths['f_eval'] + '.' + 'test'
+        predicted = learn_model.run(config.get('Learner', 'path'),
+                                x_train_path=config.get('Data', 'output') + '/' + my_prefix + 'features' + '.' + 'train' + '.tsv',
+                                x_test_path=config.get('Data', 'output') + '/' + my_prefix + 'features' + '.' + 'test' + '.tsv',
+                                y_train_path=config.get('Data', 'human') + '.' + 'train',
+                                y_test_path=config.get('Data', 'human') + '.' + 'test'
                                 )
 
-        human_scores = []
-        with open(self.paths['f_eval'] + '.' + 'test', 'r') as f:
-            lines = f.readlines()
-            for line in lines:
-                human_scores.append(float(line.strip()))
-            f.close()
+        os.remove(config.get('Data', 'output') + '/' + my_prefix + 'features' + '.' + 'train' + '.tsv')
+        os.remove(config.get('Data', 'output') + '/' + my_prefix + 'features' + '.' + 'test' + '.tsv')
 
-        print str(np.corrcoef(predicted, human_scores))
-
-        o_pred = open(self.paths['out_dir'] + '/' + prefix + '_' + 'predictions' + '.tsv', 'w')
-        for i, pred in enumerate(predicted):
-            print str(i + 1) + '\t' + str(pred) + '\t' + str(human_scores[i])
-            o_pred.write(str(i + 1) + '\t' + str(pred) + '\t' + str(human_scores[i]) + '\n')
-
-        o_pred.write(str(np.corrcoef(predicted, human_scores)) + '\n')
-        o_pred.close()
-
+        return predicted
 
     def create_samples(self, size):
 
@@ -111,9 +72,57 @@ class PredictorAbsoluteScores():
         sample_dataset.print_sampled_data(self.paths['f_ref_parse'], self.paths['f_train_phr'], self.paths['f_test_phr'], format='parsed')
         sample_dataset.print_sampled_data(self.paths['f_eval'], self.paths['f_train_phr'], self.paths['f_test_phr'], format='plain')
 
-def main():
+    @staticmethod
+    def get_human_scores(config):
+        human_scores = []
+        with open(config.get('Data', 'human') + '.' + 'test', 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                human_scores.append(float(line.strip()))
+            f.close()
+        return human_scores
+
+    @staticmethod
+    def get_features_to_extract(config):
+        features_to_extract = []
+        file_ = open(config.get('Features', 'feature_set'), 'r')
+        for line in file_:
+            features_to_extract.append(line.strip())
+        return features_to_extract
+
+def corr_feature_set(features_to_extract, set_name):
+
+    cfg = ConfigParser()
+    cfg.readfp(open(os.getcwd() + '/config/system.cfg'))
+
     predictor = PredictorAbsoluteScores()
-    predictor.run()
+    prefix = set_name
+    human_scores = predictor.get_human_scores(cfg)
+    predicted = predictor.run(prefix, cfg, features_to_extract)
+    corr = np.corrcoef(human_scores, predicted)[1][0]
+
+    return corr
+
+def main():
+
+    cfg = ConfigParser()
+    cfg.readfp(open(os.getcwd() + '/config/multi_ref.cfg'))
+
+    predictor = PredictorAbsoluteScores()
+    prefix = 'bleu_quest'
+    human_scores = predictor.get_human_scores(cfg)
+    features_to_extract = predictor.get_features_to_extract(cfg)
+    predicted = predictor.run(prefix, cfg, features_to_extract)
+    corr = np.corrcoef(human_scores, predicted)[1][0]
+    print str(corr)
+
+    o_pred = open(cfg.get('Data', 'output') + '/' + prefix + '_' + 'predictions' + '.tsv', 'w')
+    for i, pred in enumerate(predicted):
+        print str(i + 1) + '\t' + str(pred) + '\t' + str(human_scores[i])
+        o_pred.write(str(i + 1) + '\t' + str(pred) + '\t' + str(human_scores[i]) + '\n')
+
+    o_pred.write(str(np.corrcoef(predicted, human_scores)) + '\n')
+    o_pred.close()
 
 if __name__ == '__main__':
     main()
