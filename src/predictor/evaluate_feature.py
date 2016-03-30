9999ea2a08cfa6d3f43db0a5e978936fb427a03d
Marina Fomicheva <mari.fomicheva@gmail.com>
2016-03-30 12:37:42 +0200

    Added a module to evaluate separate features without training

---
+++ src/predictor/evaluate_feature.py
@@ -0,0 +1,39 @@
+__author__ = 'MarinaFomicheva'
+
+
+from src.tools.run_processors import RunProcessors
+from src.tools.prepare_wmt import PrepareWmt
+from src.features.feature_extractor import FeatureExtractor as FE
+
+""" This module scores the data using a set of selected features
+    and prints a separate wmt-formatted file for each feature.
+    For ranking, wmt dataset is prepared first."""
+
+
+def evaluate_feature_scoring(config, feature_names, data_set, lang_pair, system_name):
+
+    wmt = PrepareWmt()
+    process = RunProcessors(config)
+    sents_tgt, sents_ref = process.run_processors()
+
+    for feature in feature_names:
+        extractor = FE(config)
+        extractor.extract_features([feature], sents_tgt, sents_ref)
+        scores = [x[0] for x in extractor.vals]
+        wmt.wmt_format_simple(config, feature, data_set, lang_pair, system_name, scores)
+
+
+def evaluate_feature_ranking(config, features_to_extract):
+
+    process_wmt = PrepareWmt()
+    data_structure = process_wmt.get_data_structure(config.get('WMT', 'input_dir'))
+    process_wmt.print_data_set(config, data_structure)
+
+    process = RunProcessors(config)
+    sents_tgt, sents_ref = process.run_processors()
+
+    for feature in features_to_extract:
+        extractor = FE(config)
+        extractor.extract_features([feature], sents_tgt, sents_ref)
+        scores = [x[0] for x in extractor.vals]
+        process_wmt.wmt_format(config, feature, config.get('WMT', 'dataset'), scores, data_structure)
