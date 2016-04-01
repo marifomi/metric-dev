b6a3d1d45884b284f919f41b6496e4580841797b
Marina Fomicheva <mari.fomicheva@gmail.com>
2016-04-01 11:47:40 +0200

    Dictionary with correspondences between language pair names in wmt data from different years

---
+++ src/utils/language_codes.py
@@ -0,0 +1,12 @@
+__author__ = 'MarinaFomicheva'
+
+
+LANGUAGE_CODE_TO_NAME = {
+  'cs': 'Czech', 'de': 'German', 'fr': 'French',
+  'es': 'Spanish', 'fi': 'Finnish', 'ru': 'Russian', 'hi': 'Hindi',
+  'en': 'English',
+  'ces': 'Czech', 'deu': 'German', 'fra': 'French', 'fre': 'French',
+  'esn': 'Spanish', 'fin': 'Finnish', 'rus': 'Russian', 'hin': 'Hindi',
+  'eng': 'English'
+
+}
