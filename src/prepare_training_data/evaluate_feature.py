9102dd2ae3f855c6a066fbea729f2dc685e9d19b
Marina Fomicheva <mari.fomicheva@gmail.com>
2016-04-10 15:59:10 +0200

    Added method for printing out the evaluation results

--- src/prepare_training_data/evaluate_feature.py
+++ src/prepare_training_data/evaluate_feature.py
@@ -1,9 +1,10 @@
 __author__ = 'MarinaFomicheva'
 
 
-from src.tools.run_processors import RunProcessors
-from src.tools.prepare_wmt import PrepareWmt
+from src.processors.run_processors import RunProcessors
+from src.utils.prepare_wmt import PrepareWmt
 from src.features.feature_extractor import FeatureExtractor as FE
+import numpy
 
 """ This module scores the data using a set of selected features
     and prints a separate wmt-formatted file for each feature.
@@ -26,7 +27,7 @@ def evaluate_feature_scoring(config, feature_names, data_set, lang_pair, system_
 def evaluate_feature_ranking(config, features_to_extract):
 
     process_wmt = PrepareWmt()
-    data_structure = process_wmt.get_data_structure(config.get('WMT', 'input_dir'))
+    data_structure = process_wmt.get_data_structure(config)
     process_wmt.print_data_set(config, data_structure)
 
     process = RunProcessors(config)
@@ -37,3 +38,20 @@ def evaluate_feature_ranking(config, features_to_extract):
         extractor.extract_features([feature], sents_tgt, sents_ref)
         scores = [x[0] for x in extractor.vals]
         process_wmt.wmt_format(config, feature, config.get('WMT', 'dataset'), scores, data_structure)
+
+def print_meta_data(feature_data, feature_name, human_rankings, language_pair):
+
+    output = open('test.txt', 'w')
+
+    for pairwise_comparison in human_rankings[language_pair]:
+        value_sys1 = feature_data[pairwise_comparison.dataset, language_pair, pairwise_comparison.sys1, pairwise_comparison.phrase][feature_name]
+        value_sys2 = feature_data[pairwise_comparison.dataset, language_pair, pairwise_comparison.sys2, pairwise_comparison.phrase][feature_name]
+        difference = numpy.fabs(value_sys1 - value_sys2)
+        output.write(str(pairwise_comparison.phrase) + '\t' + pairwise_comparison.sys1 + '\t' + pairwise_comparison.sys2 +
+              '\t' + pairwise_comparison.sign +
+              '\t' + str(value_sys1) +
+              '\t' + str(value_sys2) +
+              '\t' + str(difference) + '\n'
+        )
+    output.close()
+
