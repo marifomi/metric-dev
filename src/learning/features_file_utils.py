7754f9db470ac8e023f33163b3661c21ab5ae442
marifomi <mari.fomicheva@gmail.com>
2016-04-20 18:51:26 +0200

    Added method for train/test splitting a dataset with repeated source segments (multiple translation systems)

--- src/learning/features_file_utils.py
+++ src/learning/features_file_utils.py
@@ -7,6 +7,7 @@ import codecs
 import numpy as np
 import logging as log
 import os
+import math
 from sklearn.cross_validation import train_test_split
 
 def read_labels_file(path, delim, encoding='utf-8'):
@@ -107,6 +108,39 @@ def split_dataset(input_path_x, input_path_y, output_dir):
     f.close()
 
     x_train, x_test, y_train, y_test = train_test_split(read_data_x, read_data_y)
+
+    write_lines_to_file(output_dir + '/' + 'x_train' + '.' + 'tsv', x_train)
+    write_lines_to_file(output_dir + '/' + 'y_train' + '.' + 'tsv', y_train)
+    write_lines_to_file(output_dir + '/' + 'x_test' + '.' + 'tsv', x_test)
+    write_lines_to_file(output_dir + '/' + 'y_test' + '.' + 'tsv', y_test)
+
+
+def split_dataset_repeated_segments(input_path_x, input_path_y, output_dir, number_of_segments):
+
+    with open(os.path.expanduser(input_path_x), 'r') as f:
+        read_data_x = f.readlines()
+    f.close()
+
+    with open(os.path.expanduser(input_path_y), 'r') as f:
+        read_data_y = f.readlines()
+    f.close()
+
+    segment_numbers = range(0, len(read_data_x))
+    number_of_batches = int(len(read_data_x)/number_of_segments)
+    train_length = int(round(number_of_segments * 80 / 100))
+    test_length = int(round(number_of_segments * 20 / 100))
+
+    x_train = []
+    y_train = []
+    x_test = []
+    y_test = []
+
+    for i in range(number_of_batches):
+        print('\n'.join([str(x + 1) for x in segment_numbers[i * number_of_segments + train_length:i * number_of_segments + train_length + test_length]]))
+        x_train += read_data_x[i * number_of_segments:i * number_of_segments + train_length]
+        y_train += read_data_y[i * number_of_segments:i * number_of_segments + train_length]
+        x_test += read_data_x[i * number_of_segments + train_length:i * number_of_segments + train_length + test_length]
+        y_test += read_data_y[i * number_of_segments + train_length:i * number_of_segments + train_length + test_length]
 
     write_lines_to_file(output_dir + '/' + 'x_train' + '.' + 'tsv', x_train)
     write_lines_to_file(output_dir + '/' + 'y_train' + '.' + 'tsv', y_train)
