f9fe3375320b3fe4c0aebe59aebf4422312aa333
mfomicheva <mari.fomicheva@gmail.com>
2016-06-09 16:57:47 +0200

    Added main method that prints stop word list

--- src/lex_resources/extended_stopwords_list.py
+++ src/lex_resources/extended_stopwords_list.py
@@ -7,6 +7,14 @@ class ExtendedStopwordsList(object):
     stopwords_list = []
 
     def __init__(self, language):
-        with codecs.open(os.path.expanduser('src/lex_resources/extended_stopwords/' + language + '.words'), 'r', 'utf-8') as f:
+        with codecs.open('src/lex_resources/extended_stopwords/' + language + '.words', 'r', 'utf-8') as f:
             for line in f:
                 self.stopwords_list.append(line.strip())
+
+def main():
+    stopwords = ExtendedStopwordsList('english')
+    for word in stopwords.stopwords_list:
+        print(word)
+
+if __name__ == '__main__':
+    main()
