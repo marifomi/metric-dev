5a3f55c24087ce4319db10675993bb04dac0b41e
mfomicheva <mari.fomicheva@gmail.com>
2016-07-06 16:12:13 +0200

    Created a separate module with methods for working with wmt data

--- utils/wmt.py
+++ utils/wmt.py
@@ -1,4 +1,7 @@
+""" Methods for the data in WMT format """
+
 import os
+import re
 
 
 def get_datasets(data_dir):
@@ -40,7 +43,7 @@ def reference_path(data_dir, dataset, lang_pair):
         return '/'.join([ref_dir, dataset]) + '/' + ref_name
 
     if dataset == 'newstest2013':
-        ref_name =  dataset + '-' + 'ref' + '.' + lang_pair.split('-')[1]
+        ref_name = dataset + '-' + 'ref' + '.' + lang_pair.split('-')[1]
         return ref_dir + '/' + ref_name
 
 
@@ -50,7 +53,8 @@ def system_path(data_dir, dataset, lang_pair, sys_name):
 
     if '2013' in dataset:
         sys_file = dataset + '.' + lang_pair + '.' + sys_name
-    if '2014' in dataset or '2015' in dataset or '2016' in dataset:
+    else:
+        # for 2014, 2015 and 2016 wmt
         sys_file = dataset + '.' + sys_name + '.' + lang_pair
 
     return sys_dir + '/' + sys_file
@@ -58,3 +62,27 @@ def system_path(data_dir, dataset, lang_pair, sys_name):
 
 def sentences(path):
     return sum(1 for line in open(path))
+
+
+def phrase_to_index(dataset, lang_pair, sys_name, phrase_number, data):
+    return data.plain.index((dataset, lang_pair, sys_name, phrase_number))
+
+
+def index_to_phrase(index, data):
+    return data.plain[index]
+
+
+def substitute_line_number(line, counter):
+    tokens = re.sub(r'^.+(\(.+\):)$\n', r'\1', line)
+    return 'Sentence #' + str(counter) + ' ' + tokens + '\n'
+
+
+def wmt_format(config, metric, scores, ranking_data):
+
+    with open(config.get('Paths', 'working_dir'), 'w') as o:
+
+        for i, score in enumerate(scores):
+
+            dataset, lang_pair, system_name, phrase = index_to_phrase(i, ranking_data)
+
+            o.write('{0}\t{1}\t{2}\t{3}\t{4}\t{5}\n'.format(metric, dataset, lang_pair, system_name, str(phrase), score))
