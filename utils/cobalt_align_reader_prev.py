67d32a827de8848aa922ce06cf09e5e820d1402b
Marina Fomicheva <mari.fomicheva@gmail.com>
2016-11-30 19:23:48 +0100

    Temporarily added previous cobalt aligner

---
+++ utils/cobalt_align_reader_prev.py
@@ -0,0 +1,66 @@
+import codecs
+import os
+
+
+class CobaltAlignReaderPrev(object):
+
+    def read(self, alignment_file):
+
+        alignments = []
+        lines = codecs.open(os.path.expanduser(alignment_file), 'r', 'utf-8')
+
+        for line in lines:
+            if line.startswith('Sentence #'):
+                phrase = int(line.strip().replace('Sentence #', ''))
+
+                if phrase > 1:
+                    alignments.append([indexes, words, differences])
+
+                indexes = []
+                words = []
+                differences = []
+
+            elif line.startswith('['):
+                indexes.append(CobaltAlignReaderPrev.read_alignment_idx(line))
+                words.append(CobaltAlignReaderPrev.read_alignment_words(line))
+                differences.append(CobaltAlignReaderPrev.read_differences(self, line))
+
+        alignments.append([indexes, words, differences])
+
+        lines.close()
+
+        return alignments
+
+    def read_differences(self, line):
+        values = line.strip().split(' : ')
+        context_info = {}
+
+        if len(values) < 3:
+            return context_info
+
+        context_info['srcDiff'] = self.my_split(values[2].split(';')[0].split('=')[1])
+        context_info['srcCon'] = self.my_split(values[2].split(';')[1].split('=')[1])
+        context_info['tgtDiff'] = self.my_split(values[2].split(';')[2].split('=')[1])
+        context_info['tgtCon'] = self.my_split(values[2].split(';')[3].split('=')[1])
+
+        return context_info
+
+    @staticmethod
+    def read_alignment_idx(line):
+        values = line.split(' : ')
+        return [
+            int(values[0].split(',')[0].replace('[', '')),
+            int(values[0].split(',')[1].replace(']', ''))
+        ]
+
+    @staticmethod
+    def read_alignment_words(line):
+        values = line.split(' : ')
+        return [
+            values[1].split(', ')[0].replace('[', ''),
+            values[1].split(', ')[1].replace(']', '')
+        ]
+
+    @staticmethod
+    def my_split(list):
+        return [x for x in list.split(',') if len(x) > 0]
